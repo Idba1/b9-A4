1b55f95b00e0b2e4835a3bc363516f167f0bb80b
Idba1 <moniraislamself@gmail.com>
2024-02-08 18:35:30 +0600

    push 5

--- problem.js
+++ problem.js
@@ -18,17 +18,17 @@ function calculateMoney(ticketSale) {
 
 
 function checkName(name) {
-    
+
     if (typeof name !== "string") {
         return "invalid";
     }
     else {
         name = name.trim().toLowerCase();
-        const goodNameWord = ['a', 'y','i','e','o', 'u', 'w'];
-        if(goodNameWord.includes(name.charAt(name.length-1))){
+        const goodNameWord = ['a', 'y', 'i', 'e', 'o', 'u', 'w'];
+        if (goodNameWord.includes(name.charAt(name.length - 1))) {
             return "Good Name";
         }
-        else{
+        else {
             return "Bad Name";
         }
     }
@@ -36,15 +36,56 @@ function checkName(name) {
 
 
 function deleteInvalids(array) {
-    if(!Array.isArray(array)){
+    if (!Array.isArray(array)) {
         return "Invalid Array";
 
     }
-    let newArray=[];
-    for(let i=0 ; i<array.length ; i++){
-        if(typeof (array[i]) === "number" && !isNaN(array[i])){
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (typeof (array[i]) === "number" && !isNaN(array[i])) {
             newArray.push(array[i]);
         }
     }
     return newArray;
 }
+
+
+function password(obj) {
+    if (obj.name === undefined || obj.birthYear === undefined || obj.siteName === undefined || obj.birthYear <= 999) {
+        return "invalid";
+    }
+    else {
+        const websiteName = obj.siteName.charAt(0).toUpperCase()+obj.siteName.slice(1);
+        const userName = obj.name;
+        const birth = obj.birthYear;
+        const getPassword = websiteName + "#" + userName + "@" + birth;
+        return getPassword;
+    }
+}
+console.log(password({ name: "kolimuddin", birthYear: 1999, siteName: "google" }))
+
+
+
+function monthlySavings(arr, livingCost) {
+    if(!Array.isArray(arr) || typeof livingCost !== "number"){
+        return "invalid input";
+    }
+    let totalIncome = 0;
+    for(let i=0 ; i<arr.length ; i++){
+        let payment = arr[i];
+        if(payment >= 3000){
+            totalIncome = totalIncome + payment * 0.8;
+        }
+        else{
+            totalIncome = totalIncome + payment;
+        }
+    }
+    const savings = totalIncome - livingCost;
+    if(savings >= 0){
+        return savings;
+    }
+    else{
+        return "earn more";
+    }
+}
+// console.log(monthlySavings(100, [ 900 , 2700 , 3400] ))
