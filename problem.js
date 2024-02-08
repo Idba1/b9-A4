aa120c20cc9de0c0858bb2931643f9ff200fc99a
Idba1 <moniraislamself@gmail.com>
2024-02-08 14:06:00 +0600

    push2

--- problem.js
+++ problem.js
@@ -1,21 +1,45 @@
-
 function calculateMoney(ticketSale) {
-    if(ticketSale < 0){
-        return "Please enter a positive number"
+    if (ticketSale < 0) {
+        return "Please enter a positive number";
     }
-    else{
+    else {
         const ticketPrice = 120;
         let totalTicketSalePrice = ticketPrice * ticketSale;
-        // return totalTicketSalePrice
+
         const stuffCost = 50;
-        let totalStuffCost = stuffCost*8;
-        // return totalStuffCost;
+        let totalStuffCost = stuffCost * 8;
         const gateManCost = 500;
         let totalCost = gateManCost + totalStuffCost;
-        // return totalCost;
+
         let bakerVaiHasMoney = totalTicketSalePrice - totalCost;
         return bakerVaiHasMoney;
     }
 }
-console.log(calculateMoney(10));
+
+
+function checkName(name) {
     
+    if (typeof name !== "string") {
+        return "invalid";
+    }
+    else {
+        name = name.trim().toLowerCase();
+        const goodNameWord = ['a', 'y','i','e','o', 'u', 'w'];
+        if(goodNameWord.includes(name.charAt(name.length-1))){
+            return "Good Name";
+        }
+        else{
+            return "Bad Name";
+        }
+    }
+}
+
+
+function deleteInvalids(array) {
+    if(deleteInvalids !== "array"){
+        return "invalid"
+    }else{
+        return "valid"
+    }
+}
+console.log(deleteInvalids[80]);
